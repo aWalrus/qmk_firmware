12d23b62ef68a4dd2b4d4b690174e68444b027fa
Evgeny <14215566+aWalrus@users.noreply.github.com>
2022-01-07 21:31:50 +0100

    added kyria keymap in progress for the trackball mod

---
+++ keyboards/splitkb/kyria/keymaps/awalrus-trackball/rules.mk
@@ -0,0 +1,10 @@
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+
+
+# POINTING_DEVICE_ENABLE = yes
+# POINTING_DEVICE_DRIVER = pimoroni_trackball
+
+MOUSEKEY_ENABLE = no
